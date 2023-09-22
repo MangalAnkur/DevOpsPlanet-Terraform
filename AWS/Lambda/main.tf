ba33ce565bad1b40150dff72f37a4088eab3f226
Ankur Mangal <ankur.mangal24@gmail.com>
2023-09-22 13:44:01 +0530

    Initial Commit

--- AWS/Lambda/main.tf
+++ AWS/Lambda/main.tf
@@ -1,5 +1,5 @@
 module "Lambda" {
-  source = "./modules/Lambda"
+  source        = "./modules/Lambda"
   function_name = var.function_name
   role_name     = var.role_name
   policy_name   = var.policy_name
